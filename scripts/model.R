5dca6d2b24fc996e83b3ee922eeab65afa1ebd81
David Chudzicki <dchudz@gmail.com>
2015-05-10 20:46:53 -0700

    comments

--- scripts/model.R
+++ scripts/model.R
@@ -13,12 +13,15 @@ output_file <- args[1]
 train <- read_csv(args[2])
 test <- read_csv(args[3])
 
+# process features
 train <- process_features(train)
 test <- process_features(test)
 
+# fit model
 feature_names <- c("saledate", "YearMade", "HorsePower", "ProductGroupDesc")
 rf <- randomForest(train[feature_names], train$SalePrice, ntree=10)
 
+# make predictions
 test$Predicted <- predict(rf, test[feature_names])
 test_mae <- mae$Evaluate(test$SalePrice, test$Predicted)
 
